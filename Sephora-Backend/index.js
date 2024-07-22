0c428c115e33f54194fdc08d5baf6ec9e8315d30
KaranChauhanji <karanchauhanji2003@gmail.com>
2024-07-22 06:29:12 +0530

    Backend User register and login completed DB is connected.

---
+++ Sephora-Backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+PORT = process.env.PORT || 3000;
+const connection = require("./config/db");
+const userRouter = require("./routes/user.routes");
+
+const server = express();
+
+server.use(express.json())
+server.use("/user", userRouter);
+
+
+server.get("/", (_, res) => {
+  res.status(200).send("Health check done, Server is Running.");
+});
+
+server.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Server is Running and DB is Connected.");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
